25691fc0745412889be04bbe0ee68733f1924cec
hamidreza soltani <h.soltani8452@gmail.com>
2024-05-20 00:21:18 +0330

    change color header

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,22 +1,26 @@
-
+const firstColor = "#00737e";
 function Header() {
-    
   return (
     <header>
       <div className="navbar">
         <div className="navbar-start">
-          
-          <a className="navbar-item"></a>
-          <img src="logo.png" alt="" width="50px"/>
-
+          <a className="navbar-item">
+            <img src="logo.png" alt="" width="50px" />
+          </a>
         </div>
         <div className="navbar-center">
-        <input className="input input-secondary rtl:m1-0" placeholder="جستجو در شهر" />
-
+          <input
+            className="border-firstColor input input-secondary rtl:m1-0"
+            placeholder="جستجو در شهر"
+          />
         </div>
         <div className="navbar-end">
-        <button class=" btn btn-outline-secondary">ثبت نام</button>
-          <a className="navbar-item">ورود </a>
+          <button class="btn btn-outline-secondary text-firstColor hover:bg-firstColor border-firstColor">
+            ثبت نام
+          </button>
+          <a className="hover:text-firstColor text-firstColor navbar-item">
+            ورود{" "}
+          </a>
         </div>
       </div>
     </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        firstColor: '#00737e',
+        
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":

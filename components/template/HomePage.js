bd30e948ae55258d302f2d321630cf3700e6c878
hamidreza soltani <h.soltani8452@gmail.com>
2024-05-21 00:21:38 +0330

    finaldatepicker

--- components/template/HomePage.js
+++ components/template/HomePage.js
@@ -3,8 +3,7 @@ import DatePicker from "react-multi-date-picker";
 import { useState } from "react";
 import persian from "react-date-object/calendars/persian";
 import persian_fa from "react-date-object/locales/persian_fa";
-import weekends from "react-multi-date-picker/plugins/highlight_weekends"
-
+import weekends from "react-multi-date-picker/plugins/highlight_weekends";
 
 function HomePage() {
   return (
@@ -17,10 +16,28 @@ function HomePage() {
           />
 
           <DatePicker
+            style={{
+              width: "100px",
+              height: "50px",
+              borderRadius:"12px"
+            }}
             plugins={[weekends([5, 6])]}
             calendar={persian}
             locale={persian_fa}
             placeholder="تاریخ رفت"
+          />
+
+
+<DatePicker
+            style={{
+              width: "100px",
+              height: "50px",
+              borderRadius:"12px"
+            }}
+            plugins={[weekends([5, 6])]}
+            calendar={persian}
+            locale={persian_fa}
+            placeholder="تاریخ برگشت"
           />
         </div>
       </div>

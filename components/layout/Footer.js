98884ed5a009d858458b140a45cb7d134575b115
hamidreza soltani <h.soltani8452@gmail.com>
2024-05-20 09:56:50 +0330

    final header

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,9 +1,11 @@
+import { FaBeer } from 'react-icons/fa';
+
 
 function Footer() {
   return (
     <div>
     </div>
-  )
+  );
 }
 
 export default Footer;
--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,7 +1,9 @@
-const firstColor = "#00737e";
+import { BiSupport } from "react-icons/bi";
+import { CiHeart } from "react-icons/ci";
+
 function Header() {
   return (
-    <header>
+    <header className="sticky top-0">
       <div className="navbar">
         <div className="navbar-start">
           <a className="navbar-item">
@@ -9,18 +11,37 @@ function Header() {
           </a>
         </div>
         <div className="navbar-center">
-          <input
-            className="border-firstColor input input-secondary rtl:m1-0"
+          {/* <input
+            className="border-firstColor input rtl:m1-0"
             placeholder="جستجو در شهر"
-          />
+          /> */}
+
+          
         </div>
         <div className="navbar-end">
+
+          {/* پشتیبانی */}
+          <div className="flex ml-[15px] items-center hover:text-firstColor" >
+          <BiSupport />
+          <a href="tel:09220136476" className="mr-[5px]">پشتیبانی</a>
+          </div>
+
+         {/* مورد علاقه */}
+          <div className="flex justify-center items-center ml-[15px] hover:text-firstColor">
+            <CiHeart/>
+            <a href="">موردعلاقه</a>
+          </div>
+
+
+          {/* ورود و ثبت نام */}
           <button class="btn btn-outline-secondary text-firstColor hover:bg-firstColor border-firstColor">
-            ثبت نام
+            ثبت&zwnj;نام
           </button>
           <a className="hover:text-firstColor text-firstColor navbar-item">
             ورود{" "}
           </a>
+
+          
         </div>
       </div>
     </header>

1734eb1ca19ad40313496a2f8f112678c38b11e1
hamidreza soltani <h.soltani8452@gmail.com>
2024-05-27 00:28:31 +0330

    final search ui

--- components/util/Search.js
+++ components/util/Search.js
@@ -1,22 +1,22 @@
-import React from "react";
 import DatePicker from "react-multi-date-picker";
 import persian from "react-date-object/calendars/persian";
 import persian_fa from "react-date-object/locales/persian_fa";
 import weekends from "react-multi-date-picker/plugins/highlight_weekends";
 import { FaMagnifyingGlassLocation } from "react-icons/fa6";
+
+import Style from "./Search.module.css";
 function Search() {
   return (
     <>
-      {" "}
-      <div className="w-[80%] flex justify-center">
+      <div className="  w-[90%] flex flex-col  justify-center items-center  sm:flex-row">
         <input
-          className="border-firstColor input rtl:m1-0 w-[270px] h-[50px] ml-[5px]"
+          className="border-firstColor input rtl:m1-0 w-[270px] h-[50px] ml-[5px]  "
           placeholder="جستجوی شهر، استان یا اقامتگاه"
         />
 
         <DatePicker
+          inputClass={Style.inp}
           style={{
-            width: "100px",
             height: "50px",
             borderRadius: "12px",
             marginLeft: "5px",
@@ -28,8 +28,8 @@ function Search() {
         />
 
         <DatePicker
+          inputClass={Style.inp}
           style={{
-            width: "100px",
             height: "50px",
             borderRadius: "12px",
             marginLeft: "5px",
@@ -40,8 +40,8 @@ function Search() {
           placeholder="تاریخ برگشت"
         />
 
-        <button className="bg-slate-200 h-[50px] w-[50px] flex items-center text-[25px] justify-center rounded-[12px]">
-          <FaMagnifyingGlassLocation className="text-firstColor" />
+        <button className={`bg-firstColor text-slate-200 hover:bg-slate-200 hover:text-firstColor h-[50px] w-[50px] flex items-center text-[25px] justify-center rounded-[12px] ${Style.inp} `}>
+          <FaMagnifyingGlassLocation className="" />
         </button>
       </div>
     </>
